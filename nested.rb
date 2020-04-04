06ef36df155bccf00880b926ad47b0256e1a6053
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-04 00:24:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-return programmer_hash = {:grace_hopper => {:known_for, :languages}}
+return programmer_hash = {:grace_hopper => {:known_for => [], :languages => []}}
 
 end
 
